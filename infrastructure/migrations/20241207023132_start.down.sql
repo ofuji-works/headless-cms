847dda94967975a6855c06becae5253dc3f748de
ofuji-works <ofuji.works@gmail.com>
2024-12-22 14:18:04 +0900

    feat: fix migration file for contents table

--- infrastructure/migrations/20241207023132_start.down.sql
+++ infrastructure/migrations/20241207023132_start.down.sql
@@ -1,5 +1,8 @@
 -- Add down migration script here
 DROP TRIGGER IF EXISTS content_model_updated_trigger ON content_model;
 DROP TABLE IF EXISTS content_model;
+DROP TRIGGER IF EXISTS contents_updated_trigger ON contents;
+DROP TABLE IF EXISTS contents;
 
 DROP FUNCTION set_updated_at();
+
