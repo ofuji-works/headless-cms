c95719ce41a1faf61d8635fb40a563f893bf45d6
ofuji-works <ofuji.works@gmail.com>
2025-01-19 16:17:05 +0000

    feat: add content_model domain test

---
+++ domain/src/tests/model/content_model.rs
@@ -0,0 +1,28 @@
+use crate::model::content_model::ContentModel;
+
+#[rstest::rstest]
+#[case::all_fill("a".to_string().repeat(50), "a".to_string().repeat(64), Some("a".to_string().repeat(500)))]
+#[case::without("a".to_string().repeat(50), "a".to_string().repeat(64), None)]
+fn content_model_new_success(
+    #[case] name: String,
+    #[case] api_identifier: String,
+    #[case] description: Option<String>,
+) {
+    let result = ContentModel::try_new("id".into(), name, api_identifier, description);
+
+    assert_eq!(result.is_ok(), true);
+}
+
+#[rstest::rstest]
+#[case::invalid_name("a".to_string().repeat(51), "a".to_string().repeat(64), Some("a".to_string().repeat(500)))]
+#[case::invalid_api_identifier("a".to_string().repeat(50), "a".to_string().repeat(65), Some("a".to_string().repeat(500)))]
+#[case::invalid_description("a".to_string().repeat(50), "a".to_string().repeat(64), Some("a".to_string().repeat(501)))]
+fn content_model_new_failure(
+    #[case] name: String,
+    #[case] api_identifier: String,
+    #[case] description: Option<String>,
+) {
+    let result = ContentModel::try_new("id".into(), name, api_identifier, description);
+
+    assert_eq!(result.is_err(), true);
+}
