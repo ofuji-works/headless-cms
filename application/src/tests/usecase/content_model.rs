bd9f4c8413d64ab2ce7856c82a0e75b41d07f513
ofuji-works <ofuji.works@gmail.com>
2025-01-19 09:57:33 +0000

    feat: update, delete unit test

--- application/src/tests/usecase/content_model.rs
+++ application/src/tests/usecase/content_model.rs
@@ -2,7 +2,9 @@ use std::sync::Arc;
 
 use domain::{
     model::content_model::ContentModel,
-    repository::content_model::{CreateContentModel, MockContentModelRepository},
+    repository::content_model::{
+        CreateContentModel, MockContentModelRepository, UpdateContentModel,
+    },
 };
 
 use crate::usecase::content_model::ContentModelUsecase;
@@ -10,11 +12,6 @@ use crate::usecase::content_model::ContentModelUsecase;
 #[rstest::fixture]
 fn content_model() -> ContentModel {
     ContentModel::try_new("id".into(), "name".into(), "api_identifier".into(), None).unwrap()
-}
-
-#[rstest::fixture]
-fn create_content_model() -> CreateContentModel {
-    CreateContentModel::new("name".into(), "api_identifier".into(), None)
 }
 
 #[tokio::test]
@@ -40,6 +37,48 @@ async fn create_success(#[case] create_content_model: CreateContentModel) {
     });
     let usecase = ContentModelUsecase::new(Arc::new(mock));
     let result = usecase.create(create_content_model).await;
+
+    assert_eq!(result.is_ok(), true);
+}
+
+#[tokio::test]
+#[rstest::rstest]
+#[case::simple(UpdateContentModel::new("id".into(), Some("new_name".into()), Some("new_api_identifier".into()), None))]
+async fn update_success(
+    content_model: ContentModel,
+    #[case] update_content_model: UpdateContentModel,
+) {
+    let mut mock = MockContentModelRepository::new();
+    mock.expect_update().returning(move |x| {
+        let name = match x.name {
+            Some(name) => name,
+            None => content_model.name.clone(),
+        };
+        let api_identifier = match x.api_identifier {
+            Some(api_identifier) => api_identifier,
+            None => content_model.api_identifier.clone(),
+        };
+        let description = match x.description {
+            Some(description) => Some(description),
+            None => content_model.description.clone(),
+        };
+
+        ContentModel::try_new(content_model.id.clone(), name, api_identifier, description)
+    });
+
+    let usecase = ContentModelUsecase::new(Arc::new(mock));
+    let result = usecase.update(update_content_model).await;
+
+    assert_eq!(result.is_ok(), true);
+}
+
+#[tokio::test]
+#[rstest::rstest]
+async fn delete_success() {
+    let mut mock = MockContentModelRepository::new();
+    mock.expect_delete().returning(|_| Ok(()));
+    let usecase = ContentModelUsecase::new(Arc::new(mock));
+    let result = usecase.delete("id".into()).await;
 
     assert_eq!(result.is_ok(), true);
 }
